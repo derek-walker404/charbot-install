4f9fd68c30cbd8f30452457387e7c860b29f01f1
desposi1 <david@Davids-MacBook-Pro.local>
2015-05-17 21:18:05 -0400

    Initial Commit. Deploy ready.

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+cd ../tpofof-core/;mvn clean install; cd ../charbot-install/
+cd ../charbox-domain/;mvn clean install; cd ../charbot-install/
+cd ../charbot-install/;mvn clean install;cd ../charbot-install/
---
+++ src/main/java/co/charbox/install/InstallMain.java
@@ -0,0 +1,59 @@
+package co.charbox.install;
+
+import java.io.IOException;
+
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import co.charbox.domain.model.auth.DeviceAuthModel;
+
+import com.tpofof.core.App;
+import com.tpofof.core.utils.AuthorizationHeader;
+import com.tpofof.core.utils.Config;
+import com.tpofof.core.utils.HttpClientProvider;
+import com.tpofof.core.utils.json.JsonUtils;
+
+@Component
+public class InstallMain {
+
+	@Autowired private HttpClientProvider httpClientProvider;
+	@Autowired private Config config;
+	@Autowired private JsonUtils json;
+	
+	public void installDeviceCredentials() {
+		String serviceId = config.getString("install.service.id");
+		String serviceApiKey = config.getString("install.service.apikey");
+		String serviceGroup = config.getString("install.service.group");
+		DeviceAuthModel newAuth = getNewAuth(serviceId, serviceApiKey, serviceGroup);
+		if (newAuth != null) {
+			config.setProperty("device.id", newAuth.getDeviceId());
+			config.setProperty("device.api.key", newAuth.getApiKey());
+			config.setProperty("install.service.id", "");
+			config.setProperty("install.service.apikey", "");
+			config.setProperty("install.service.group", "");
+		}
+	}
+	
+	protected DeviceAuthModel getNewAuth(String serviceId, String serviceApiKey, String serviceGroup) {
+		PostMethod pm = new PostMethod(config.getString("charbot.api.uri", "http://localhost:8080") + "/install");
+		pm.addRequestHeader(new AuthorizationHeader(serviceId, serviceApiKey, serviceGroup));
+		try {
+			if (200 == httpClientProvider.get().executeMethod(pm)) {
+				return json.fromJsonResponse(pm.getResponseBodyAsString(), DeviceAuthModel.class);
+			}
+		} catch (HttpException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			pm.releaseConnection();
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		App.getContext().getBean(InstallMain.class).installDeviceCredentials();
+	}
+}
