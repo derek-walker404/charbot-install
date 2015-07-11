94fa96cbf112ffa1f62a7fa247a20e41f9dfb749
David Esposito <aCommonDev@gmail.com>
2015-07-11 09:31:34 -0400

    'Adding installation support'

---
+++ install-auth.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'install.service.id = client-installer-charbot' >> app.properties
+echo 'install.service.apikey = 4wt589jhergfh3' >> app.properties
+echo 'install.service.group = install' >> app.properties
---
+++ setup_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./clean.sh
+
+rm charbot.sh
+ln /home/odroid/charbot_install.sh charbot.sh
