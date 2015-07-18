03a79c78987ee93c71c3341574b0799f822fcc1f
David Esposito <aCommonDev@gmail.com>
2015-07-18 08:27:02 -0400

    'Updating resources to use Response object. Converting DAO to new Sort Mapping type. Updating install script.'

--- install-client.sh
+++ install-client.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 wget --no-check-certificate https://s3-us-west-1.amazonaws.com/charbot-dl/client/charbot-install-0.1.0-SNAPSHOT.jar &>> install.log
-wget --no-check-certificate https://s3-us-west-1.amazonaws.com/charbot-dl/client/charbox-client-0.1.3-SNAPSHOT.jar &>> install.log
-
-./install-client.sh
-rm install-client.sh
+wget --no-check-certificate https://s3-us-west-1.amazonaws.com/charbot-dl/client/charbox-client-0.1.4-SNAPSHOT.jar &>> install.log
 
 java -jar charbot-install-0.1.0-SNAPSHOT.jar &>> install.log
 
 rm charbot-install-0.1.0-SNAPSHOT.jar
 
-cp charbot_run.sh charbot_0.1.3.sh
-echo 'java -jar charbox-client-0.1.3-SNAPSHOT.jar client &' >> charbot_0.1.3.sh
+cp charbot_run.sh charbot_0.1.4.sh
+echo 'java -jar charbox-client-0.1.4-SNAPSHOT.jar client &' >> charbot_0.1.4.sh
 rm charbot.sh
-ln /home/odroid/charbot_0.1.3.sh charbot.sh
+ln /home/odroid/charbot_0.1.4.sh charbot.sh
 
 ./charbot.sh
